77138bc3a4e8009b6d30139abe8412074ef72b8b
Abdul Moiz <138908943+codewithmoiz@users.noreply.github.com>
2024-06-17 10:13:28 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Lenis JS
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  });
+// const lenis = new Lenis({
+//     duration: 1.2,
+//     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+//   });
   
-  lenis.start(); // Initialize Lenis
+//   lenis.start(); // Initialize Lenis
   
-  function raf(time) {
-    lenis.raf(time);
-    ScrollTrigger.update();
-    requestAnimationFrame(raf);
-  }
+//   function raf(time) {
+//     lenis.raf(time);
+//     ScrollTrigger.update();
+//     requestAnimationFrame(raf);
+//   }
   
-  requestAnimationFrame(raf);
+//   requestAnimationFrame(raf);
 
 
 // Main Code
